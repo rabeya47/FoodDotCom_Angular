026ac126da02828dccc281d0922c7871e81633a0
rabeya47 <rabu016833@gmail.com>
2022-03-23 20:32:27 +0600

    23/03/2022

---
+++ src/app/components/adminlayout/admin/orderlist/cart.model.ts
@@ -0,0 +1,9 @@
+export class cart{
+    id: number = 0;
+    productName: String = "";
+    quantity: number = 0;
+    price: number = 0;
+    images: string = '';
+
+    constructor(){}
+}
--- src/app/components/adminlayout/admin/orderlist/orderlist.component.ts
+++ src/app/components/adminlayout/admin/orderlist/orderlist.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { cart } from './cart.model';
 import { Ordertable } from './orderlist.model';
 
 @Component({
@@ -9,14 +10,18 @@ import { Ordertable } from './orderlist.model';
   styleUrls: ['./orderlist.component.css']
 })
 export class OrderlistComponent implements OnInit {
-  order: Ordertable = new Ordertable();
+  //order: Ordertable = new Ordertable();
+  cart:  cart = new cart();
+
   cancel: boolean = false;
   delivery: boolean = false;
 
   constructor(private http:HttpClient, private router:Router) { }
 
-Orders:any;
-
+//Orders:any;
+carts : any;
+total=0;    
+value:any;
 
 
   ngOnInit(): void {
@@ -24,17 +29,18 @@ Orders:any;
   }
 
 
+  
+
 
  show() {
     const headers = { 'content-type': 'application/json' };
-    this.http.get<any>('http://localhost:8081/order/getAll', { headers })
+    this.http.get<any>('http://localhost:8081/cart/getAll', { headers })
       .subscribe(map => {
 
-        this.Orders = map.data;
-
+        this.carts = map.data;
+        this.findsum(this.carts)
       })
   }
-
 
 
   cancelOrder(order:any) {
@@ -56,6 +62,20 @@ Orders:any;
         this.show();
       }
       )
+  }
+
+  
+  findsum(data: string | any[]){    
+    debugger  
+    this.value=data    
+    console.log(this.value); 
+     
+    for(let j=0;j<data.length;j++){ 
+      
+         this.total+= this.value[j].price  
+         console.log(this.total)  
+    } 
+    console.log(this.total)  
   }
 
 }
